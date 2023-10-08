e3d6023588e6696bf078325ca26703857bf28db6
mehedi08cloud <mehedimurad08@gmail.com>
2023-10-09 01:14:33 +0600

    final commit

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCGJfsm8JyhzphyOkD2ajM8yKNz5YtwrW0",
-  authDomain: "tech-landing-page-assignment9.firebaseapp.com",
-  projectId: "tech-landing-page-assignment9",
-  storageBucket: "tech-landing-page-assignment9.appspot.com",
-  messagingSenderId: "999820106374",
-  appId: "1:999820106374:web:c3e157ef1fb06c6c7f8f07"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
